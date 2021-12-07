7da2236c805c0cefcc27e37fcad48a2211df138c
kamelfridhi <80178238+kamelfridhi@users.noreply.github.com>
2021-12-07 21:50:51 +0100

    dossier viewer admin user

---
+++ Viewer/bloquer.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "vous etes bloquez";
+
+?>
---
+++ Viewer/verif.php
@@ -0,0 +1,7 @@
+<?php
+	// include '../Controler/clientC.php';
+	// $clientC=new clientC();
+    // require_once "../mailer/mail.php";
+    // $clientC->activecompte($client,$_POST['username']);
+	//header('Location:login.php');
+?>
