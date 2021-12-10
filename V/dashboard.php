d24f461203ca26d9c23a7968d54eaa761a82921a
samar0507 <79337944+samar0507@users.noreply.github.com>
2021-12-10 15:37:53 +0100

    Update dashboard.php

--- V/dashboard.php
+++ V/dashboard.php
@@ -403,12 +403,13 @@ $angle=$pourcentage*3.6;
     <!-- End Navbar -->
 
 
-    <div class="container-fluid py-4">
-      <div class="row">
+    <div align="center">
+  <div class="row">
         <div class="col-12">
           <div class="card mb-4">
-          <h6>  Reclamation's chart</h6>
-
+            <div class="card-header pb-0">
+            <br>
+            <h4>Reclamation's chart</h4>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
               <table class="table align-items-center mb-0">
@@ -427,7 +428,7 @@ $angle=$pourcentage*3.6;
 </thead>
 </table>
 
-
+            </div>
               </div>
             </div>
           </div>
