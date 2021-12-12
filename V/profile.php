209b01cc94e7b47884cb5272defb2c5fe641b005
berhoumaeya <80199721+berhoumaeya@users.noreply.github.com>
2021-12-12 12:50:22 +0100

    Update profile.php

--- V/profile.php
+++ V/profile.php
@@ -22,7 +22,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icone.png">
   <link rel="icon" type="image/png" href="../assets/img/favicone.png">
   <title>
-    Soft UI Dashboard by Creative Tim
+    BuyTech
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -42,7 +42,7 @@
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html" target="_blank">
         <img src="../assets/img/logo-cte.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Soft UI Dashboard</span>
+        <span class="ms-1 font-weight-bold">BuyTech</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
